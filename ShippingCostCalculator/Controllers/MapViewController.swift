6c73fc174302fa0838fa1299693abfca75b1cc08
PLUXVR <pluxar@bk.ru>
2022-04-13 22:12:59 +0300

    initial commit

---
+++ ShippingCostCalculator/Controllers/MapViewController.swift
@@ -0,0 +1,235 @@
+//
+//  MapViewController.swift
+//  ShippingCostCalculator
+//
+//  Created by Максим Беседин on 16.03.2022.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+class MapViewController: UIViewController {
+    
+    var routeDistance : Double?
+    
+    @IBOutlet weak var MapViewOutlet: MKMapView!
+
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    @objc let addAdressButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "addAdress"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let resetButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "resetButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    let routeButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "routeButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    let stepBack : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "stepBack"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let stepNext : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "stepNext"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var annotationsArray = [MKPointAnnotation]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView.delegate = self
+        
+        setConstraints()
+
+        addAdressButton.addTarget(self, action: #selector(addAdresButtonTaped), for: .touchUpInside)
+    
+        routeButton.addTarget(self, action: #selector(routeButtonTaped), for: .touchUpInside)
+    
+        resetButton.addTarget(self, action: #selector(resetButtonTaped), for: .touchUpInside)
+        
+        stepBack.addTarget(self, action: #selector(stepBackButtonTaped), for: .touchUpInside)
+
+        stepNext.addTarget(self, action: #selector(stepNexButtonTaped), for: .touchUpInside)
+    }
+    
+    @objc func addAdresButtonTaped(){
+        alertAddAdress(title: "Добавить", placeholder: "Введите адрес") { [self] (text) in
+            self.setupPlacemark(adressPlace: text)
+        }
+    }
+    
+    @objc func routeButtonTaped(){
+        
+        
+        for index in 0...annotationsArray.count - 1 {
+            createDirectionRequest(startCoordinate: annotationsArray[index].coordinate, destinationCoordinate: annotationsArray[index].coordinate)
+        }
+        
+        mapView.showAnnotations(annotationsArray, animated: true)
+        
+    }
+    
+    @objc func resetButtonTaped(){
+        mapView.removeOverlays(mapView.overlays)
+        mapView.removeAnnotations(mapView.annotations)
+        annotationsArray = [MKPointAnnotation]()
+        routeButton.isHidden = true
+        resetButton.isHidden = true
+    }
+    
+    @objc func stepBackButtonTaped(){
+        print(routeDistance)
+    }
+    
+    @objc func stepNexButtonTaped(sender: UIButton){
+        let calculateController = storyboard?.instantiateViewController(withIdentifier: "CalculatingViewController")
+        let newControler = calculateController as! CalculatingViewController
+        newControler.distance = 100
+        self.navigationController?.pushViewController(calculateController!, animated: true)
+    }
+    
+    private func setupPlacemark(adressPlace: String){
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(adressPlace) { [self] placemarks, error in
+            if let error = error {
+                print(error)
+                alertError(title: "Ошибка", message: "Попробуйте добавить адрес еще раз")
+            }
+            
+            guard let placemarks = placemarks else {return}
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = "\(adressPlace)"
+            guard let plcemarkLocation = placemark?.location else {return}
+            annotation.coordinate = plcemarkLocation.coordinate
+            annotationsArray.append(annotation)
+            
+            if annotationsArray.count > 1 {
+                routeButton.isHidden = false
+                resetButton.isHidden = false
+            }
+            mapView.showAnnotations(annotationsArray, animated: true)
+            }
+        
+        }
+    
+    private func createDirectionRequest(startCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D){
+        let startLocation = MKPlacemark(coordinate: startCoordinate)
+        let destinationLocation = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startLocation)
+        request.destination = MKMapItem(placemark: destinationLocation)
+        request.transportType =  .walking
+        request.requestsAlternateRoutes = true
+        
+        
+        let direction = MKDirections(request: request)
+        direction.calculate { responce, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let responce = responce else {
+                self.alertError(title: "Ошибка", message:  "Маршрут недоступен")
+                return
+                }
+            var minRoute = responce.routes[0]
+            for route in responce.routes{
+                minRoute = (route.distance < minRoute.distance) ? route: minRoute
+                }
+            self.routeDistance = minRoute.distance
+            self.mapView.addOverlay(minRoute.polyline)
+            
+            }
+        }
+    }
+
+extension MapViewController : MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        renderer.strokeColor = .blue
+        return renderer
+    }
+}
+
+extension MapViewController {
+    
+    func setConstraints(){
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+            mapView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+
+        mapView.addSubview(addAdressButton)
+        NSLayoutConstraint.activate([
+            addAdressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAdressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAdressButton.heightAnchor.constraint(equalToConstant:  70),
+            addAdressButton.widthAnchor.constraint(equalToConstant: 70)
+        ])
+
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            resetButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            resetButton.heightAnchor.constraint(equalToConstant:  70),
+            resetButton.widthAnchor.constraint(equalToConstant: 70)
+        ])
+
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 175),
+            routeButton.heightAnchor.constraint(equalToConstant:  70),
+            routeButton.widthAnchor.constraint(equalToConstant: 70)
+        ])
+        
+        mapView.addSubview(stepBack)
+        NSLayoutConstraint.activate([
+            stepBack.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            stepBack.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            stepBack.heightAnchor.constraint(equalToConstant:  60),
+            stepBack.widthAnchor.constraint(equalToConstant: 60)
+        ])
+
+        mapView.addSubview(stepNext)
+        NSLayoutConstraint.activate([
+            stepNext.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            stepNext.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            stepNext.heightAnchor.constraint(equalToConstant:  60),
+            stepNext.widthAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+}
+
+
---
+++ ShippingCostCalculator/Controllers/ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  ShippingCostCalculator
+//
+//  Created by Максим Беседин on 07.03.2022.
+//
+
+import UIKit
+
+class CalculatingViewController: UIViewController {
+    
+    //Здесь будет храниться результат расчета суммы заказа для отображения в финальном Label
+    var cdekValue = "0.0"
+    var distance : Int = 0
+    // дефолтная цена за параметр объекта
+    var defaultParamPrice: Int = 200
+    
+    var paramPriceMatrix: [Array<Double>: Int] = [[0, 1]: 0,
+                                                  [2, 5]: 10,
+                                                  [5, 10]: 15,
+                                                  [10, 15]: 20,
+                                                  [15, 20]: 25,
+                                                  [20, 25]: 30,
+                                                  [25, 35]: 35,
+                                                  [35, 45]: 40,
+                                                  [45, 60]: 45,
+                                                  [60, 75]: 50,
+                                                  [75, 90]: 55,
+                                                  [90, 110]: 60,
+                                                  [110, 130]: 65,
+                                                  [130, 150]: 70,
+                                                  [150, 200]: 90,]
+    
+    let weightArray : [String] = [
+        "До 2 кг",
+        "До 5 кг",
+        "До 12 кг",
+        "До 20 кг",
+        "До 50 кг",
+        "До 199 кг",
+     
+    ]
+    //Здесь будет храниться выбор пользователя в pickerView
+    var pickerValue = ""
+    
+    //Добавляем аутлеты для view
+    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet var mainView: UIView!
+    
+    //Добавляем аутлеты для slider
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var widthSlider: UISlider!
+    @IBOutlet weak var lengthSlider: UISlider!
+    
+    //Добавляем аутлеты для label
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var widthLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    
+    //Добавляем  outlet для picker
+    @IBOutlet weak var pickerOutlet: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let picker = pickerOutlet
+        
+        //Подписываемся на протокол dataSource (Пока не знаю что это, надо изучить)
+        picker?.dataSource = self
+        
+        picker?.delegate = self
+        
+        //Задаем закгругление у whiteView
+        whiteView?.layer.cornerRadius = 30
+        
+        //Скрываем нижние закругленные края у whiteView
+        whiteView?.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+    }
+    // Добавляем  action для слайдера с шириной
+    @IBAction func heightValue(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.1f", sender.value) + String(" см")
+    }
+    // Добавляем  action для слайдера с высотой
+    @IBAction func widthValue(_ sender: UISlider) {
+        widthLabel.text = String(format: "%.1f", sender.value) + String(" см")
+    }
+    // Добавляем  action для слайдера с длиной
+    @IBAction func lengthValueFunction(_ sender: UISlider) {
+        lengthLabel.text = String(format: "%.1f", sender.value) + String(" см")
+    }
+    
+    // Добавляем  action для кнопки расчета стоимости доставки
+    @IBAction func calculateButton(_ sender: UIButton) {
+    //Переменные в которых будут храниться значения слайдеров
+        let height = heightSlider.value
+        let width = widthSlider.value
+        let length = lengthSlider.value
+    
+    //Переменная в которую мы записываем результат расчетов по стоимости доставки
+        var cdek : Float
+        
+    //Вместо привычного if будем использовать switch, так как вариантов много и каждый нужно проверить
+        switch pickerValue {
+        case "До 2 кг" : cdek = (height * width * length + 250) / 100
+        case "До 5 кг" : cdek = (height * width * length + 400) / 100
+        case "До 12 кг" : cdek = (height * width * length + 600) / 100
+        case "До 20 кг" : cdek = (height * width * length + 850) / 100
+        case "До 50 кг" : cdek = (height * width * length + 1500) / 100
+        case "До 199 кг" : cdek = (height * width * length + 200) / 100
+        default: cdek = 0.0
+        }
+        print(distance)
+        cdekValue = String(format: "%.1f", cdek) + String(" рублей")
+        
+        print (cdekValue)
+        //Переход на mapViewController
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+//      self.performSegue(withIdentifier: "goToMap", sender: self) //goToResult
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationView = segue.destination as! ResultViewController
+            destinationView.result = cdekValue
+        }
+    }
+    
+}
+
+extension CalculatingViewController: UIPickerViewDataSource {
+    
+    //Сколько компонентов выводить в пикер
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    //Сколько строк в компоненте в пикере
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 6
+    }
+}
+
+extension CalculatingViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // Фиксируем выбор пользователя в переменную pickerValue
+        pickerValue = weightArray[row]
+        
+        return weightArray[row]
+            
+            }
+}
+
+ 
