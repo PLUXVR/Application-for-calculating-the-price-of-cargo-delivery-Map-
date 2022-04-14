5191b24eb468b2e06cd91e86e75cc59c6795ce1f
Pation Prime <pationprime@gmail.com>
2022-04-14 22:27:45 +0300

    fixes

---
+++ ShippingCostCalculator/Controllers/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  ShippingCostCalculator
+//
+//  Created by Максим Беседин on 13.03.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var result : String?
+
+    @IBOutlet weak var resultOutlet: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        resultOutlet.text = result!
+    }
+    
+
+    @IBAction func recalculateButton(_ sender: UIButton) {
+//        self.dismiss(animated: true, completion: nil)
+        
+        self.performSegue(withIdentifier: "goToMap", sender: self)
+
+    }
+    
+}
+
